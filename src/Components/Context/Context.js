337a9f60991f097c483921d37fc79e0b5f4bc56b
AbubakrRehman <abubakr.rehman91@outlook.com>
2023-02-04 01:05:38 +0530

    faket changed to fetch call

--- src/Components/Context/Context.js
+++ src/Components/Context/Context.js
@@ -1,6 +1,6 @@
 // import { createContext } from "react";
 import { faker } from '@faker-js/faker';
-import React, { createContext, useContext, useReducer } from 'react';
+import React, { createContext, useContext, useEffect, useReducer, useState } from 'react';
 import cartReducer from '../reducers/cartReducer';
 import filterReducer from '../reducers/filterReducer';
 // faker.seed(99);
@@ -11,17 +11,33 @@ const Cart = createContext();
 function Context({ children }) {
 
 
-    const products = [...Array(20)].map((item, index) => {
-        return {
-            "name": faker.commerce.product(),
-            "price": faker.commerce.price(),
-            "fashion": faker.image.abstract(),
-            "id": faker.datatype.uuid(),
-            "Dept": faker.commerce.department(),
-            "ratings":faker.datatype.number({ min: 1, max: 5, precision: 1}) ,
-            "inStock":faker.datatype.boolean()
-        }
-    });
+    // const products = [...Array(20)].map((item, index) => {
+    //     return {
+    //         "name": faker.commerce.product(),
+    //         "price": faker.commerce.price(),
+    //         "fashion": faker.image.abstract(),
+    //         "id": faker.datatype.uuid(),
+    //         "Dept": faker.commerce.department(),
+    //         "ratings":faker.datatype.number({ min: 1, max: 5, precision: 1}) ,
+    //         "inStock":faker.datatype.boolean()
+    //     }
+    // });
+    const [products, setProducts] = useState({});
+    useEffect(()=>{
+        let products={};
+        fetch('https://dummyjson.com/products')
+        .then(res=>res.json())
+        .then(json=>{
+            console.log(json);
+            products["name"]=json.title;
+            products["price"]=json.price;
+            products["fashion"]=json.image;
+            products["id"]=json.id;
+            products["Dept"]=json.category;
+            products["inStock"]=json.stock?true:false;
+            setProducts(products);
+        })
+    },[]);
 
     
     const [state, dispatch] = useReducer(cartReducer, {
